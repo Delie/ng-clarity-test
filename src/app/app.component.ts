a0a998b5043108e5a956a9ecb2b9ed3a45b78955
Andrew Delicata <adelicata@hotmail.com>
2018-11-08 22:41:54 +0000

    Angular date test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-date-picker-test';
+export class AppComponent implements OnInit {
+  public fgDateForm: FormGroup;
+
+  public ngOnInit(): void {
+    this.setupForm();
+  }
+
+  public setupForm(): void {
+    this.fgDateForm = new FormGroup({
+      fcDate: new FormControl(''),
+      fcDate2: new FormControl(''),
+    });
+    this.fgDateForm.controls.fcDate.setValue('2018-11-03');
+    this.fgDateForm.controls.fcDate2.setValue('2018-11-03');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { ClarityModule } from '@clr/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import localeEnGb from '@angular/common/locales/en-GB';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEnGb, 'en-GB');
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'en-GB' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
